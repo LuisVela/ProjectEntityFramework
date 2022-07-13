e27161a3a229ea667f33a50dbc6f908455bb9013
Luis Andrés Vela <luisve201@gmail.com>
2022-07-13 11:41:01 -0500

    Add Model Task

---
+++ Models/Task.cs
@@ -0,0 +1,18 @@
+namespace ProjectEF.Models;
+
+public class Task
+{
+    public Guid TaskId { get; set; }
+    public Guid CategoryId { get; set; }
+    public string Tittle { get; set; }
+    public string Description { get; set; }
+    public Priority TaskPriority { get; set; }
+    public DateTime CreationDate { get; set; }
+}
+
+public enum Priority
+{
+    Short,
+    Half,
+    High
+}
