bd3ca0eff43a73b5a9929fd6749d9c1ed878300f
Luis Andrés Vela <luisve201@gmail.com>
2022-07-13 15:17:21 -0500

    Update (Mapping Model)

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectEF.Models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string Name { get; set; }
+
     public string Description { get; set; }
+    
     public virtual ICollection<Job> Jobs { get; set; }
 }
--- Models/Job.cs
+++ Models/Job.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ProjectEF.Models;
 
 public class Job
 {
+    [Key]
     public Guid TaskId { get; set; }
+
+    [ForeignKey("CtegoryId")]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Tittle { get; set; }
+
     public string Description { get; set; }
+
     public Priority TaskPriority { get; set; }
+
     public DateTime CreationDate { get; set; }
+
     public virtual Category Category { get; set; }
+
+    [NotMapped]
+    public string Resumen { get; set; }
 }
 
 public enum Priority
