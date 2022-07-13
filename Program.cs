16cd0aff0927e4e1ce0cf869e8f2f05c05dcab9e
Luis Andrés Vela <luisve201@gmail.com>
2022-07-13 16:21:03 -0500

    Update Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectEF;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<JobContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", () => async ([FromServices] JobContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
